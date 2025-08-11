d0c301328726af0f8fb2dd31642165a7abb88ece
luiz fellipe <luiz.13.silva@gmail.com>
2025-08-11 17:49:41 -0400

    Adjusted secrets on project

--- src/main/java/com/spring/objectstore/configs/MinioClientConfig.java
+++ src/main/java/com/spring/objectstore/configs/MinioClientConfig.java
@@ -1,17 +1,24 @@
 package com.spring.objectstore.configs;
 
 import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinioClientConfig {
 
+    @Value("${minio.user}")
+    private String user;
+
+    @Value("${minio.password}")
+    private String password;
+
     @Bean
     MinioClient minioClient() {
         return MinioClient.builder()
                 .endpoint("http://localhost:9000")
-                .credentials("root", "minioadmin")
+                .credentials(user, password)
                 .build();
     }
 }
